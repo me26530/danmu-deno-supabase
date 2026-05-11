7f21954264f9ccbd87f8b5ad163979b5a6c2f663
me26530 <61023195+me26530@users.noreply.github.com>
2026-05-11 17:11:35 +0800

    Update index.ts

--- supabase/functions/danmu/index.ts
+++ supabase/functions/danmu/index.ts
@@ -1,29 +1,47 @@
-import "jsr:@supabase/functions-js/edge-runtime.d.ts"
+import "jsr:@supabase/functions-js/edge-runtime.d.ts";
+import { handler } from "../../../main.ts";
+
+function withCors(response: Response): Response {
+  const headers = new Headers(response.headers);
+
+  headers.set("access-control-allow-origin", "*");
+  headers.set("access-control-allow-methods", "GET, POST, OPTIONS");
+  headers.set(
+    "access-control-allow-headers",
+    "authorization, x-client-info, apikey, content-type",
+  );
+
+  return new Response(response.body, {
+    status: response.status,
+    statusText: response.statusText,
+    headers,
+  });
+}
+
+function rewriteRequestForDanmu(req: Request): Request {
+  const url = new URL(req.url);
+
+  if (url.pathname.startsWith("/danmu")) {
+    url.pathname = url.pathname.replace(/^\/danmu/, "") || "/";
+  }
+
+  return new Request(url.toString(), req);
+}
 
 Deno.serve(async (req) => {
-  const url = new URL(req.url)
-
   if (req.method === "OPTIONS") {
     return new Response("ok", {
       headers: {
         "access-control-allow-origin": "*",
         "access-control-allow-methods": "GET, POST, OPTIONS",
-        "access-control-allow-headers": "authorization, x-client-info, apikey, content-type",
+        "access-control-allow-headers":
+          "authorization, x-client-info, apikey, content-type",
       },
-    })
+    });
   }
 
-  return new Response(
-    JSON.stringify({
-      ok: true,
-      message: "danmu supabase function running",
-      path: url.pathname,
-    }),
-    {
-      headers: {
-        "content-type": "application/json; charset=utf-8",
-        "access-control-allow-origin": "*",
-      },
-    },
-  )
-})
+  const rewrittenReq = rewriteRequestForDanmu(req);
+  const response = await handler(rewrittenReq);
+
+  return withCors(response);
+});
